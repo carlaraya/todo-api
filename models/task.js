6abd2704f07b220452a1572da96b331defddcc5d
carlaraya <carl.araya256@gmail.com>
2019-02-23 09:29:33 +0800

    default done field

--- models/task.js
+++ models/task.js
@@ -4,8 +4,9 @@ const Schema = mongoose.Schema;
 
 var TaskSchema = Schema({
   name: {type: String, required: true, max: 100},
-  done: {type: Boolean, required: true},
-  notes: {type: String, max: 300}
+  done: {type: Boolean, default: false},
+  notes: {type: String, max: 300},
+  priority: {type: Number, min: 0, max: 1000}
 });
 
 TaskSchema.plugin(timestamps);
