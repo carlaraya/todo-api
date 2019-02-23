4b52238d2a390de92b17f1814f641fc11a9c3d5e
carlaraya <carl.araya256@gmail.com>
2019-02-23 09:51:50 +0800

    Show documentation in root page /

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const routes = require('express').Router();
+const showdown = require('showdown');
+const fs = require('fs');
+const converter = new showdown.Converter({completeHTMLDocument: true});
+converter.setFlavor('github');
 
 routes.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to my API!' });
+  fs.readFile('README.md', 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data)
+    readmeHtml = converter.makeHtml(data);
+    console.log(readmeHtml);
+    res.status(200).send(readmeHtml);
+  });
 });
 
 module.exports = routes;
