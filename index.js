9912e2cfbb7145620b61f7b0d1e6f7eeda5d2012
carlaraya <carl.araya256@gmail.com>
2019-03-10 13:54:44 +0800

    cors

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const app = require('express')();
 const formidable = require('express-formidable');
+const cors = require('cors');
+
 const routes = require('./routes');
 const tasksRoutes = require('./routes/tasks');
 
@@ -10,6 +12,12 @@ const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.use(formidable());
+app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/', routes);
 app.use('/tasks', tasksRoutes);
 
